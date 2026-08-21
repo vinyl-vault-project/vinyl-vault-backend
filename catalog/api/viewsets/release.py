663bbaa585a10939723ce4cee96138f6bf36ac70
Yuliia Vasylieva <qellexi@gmail.com>
2026-08-21 18:27:05 +0300

    feat: add pagination to release viewset and custom pagination class

--- catalog/api/viewsets/release.py
+++ catalog/api/viewsets/release.py
@@ -2,6 +2,7 @@ from django.db.models import Min, Q
 from rest_framework import permissions, viewsets, mixins
 from rest_framework.exceptions import PermissionDenied
 
+from catalog.api.pagination import ReleasePagination
 from catalog.api.serializers.release import SavedReleaseSerializer, ReleaseListSerializer, ReleaseDetailSerializer
 from catalog.models import SavedRelease, Release
 
@@ -11,6 +12,7 @@ class ReleaseViewSet(mixins.ListModelMixin, mixins.RetrieveModelMixin, viewsets.
     GET /api/albums/:slug  (Album Page)."""
 
     lookup_field = "slug"
+    pagination_class = ReleasePagination
 
     def get_serializer_class(self):
         if self.action == "retrieve":
